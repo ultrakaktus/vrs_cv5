cf22336959443919ea0e0750d9b80cb4d2091a6f
ultrakaktus <jkozarik@gmail.com>
2016-10-17 15:12:36 +0200

    cv5 commit 1
    zatial len kopriovacky z minuleho cvika a z prednasky

---
+++ Libraries/vrs_cv5.c
@@ -0,0 +1,8 @@
+/*
+ * vrs_cv5.c
+ *
+ *  Created on: 17. 10. 2016
+ *      Author: J
+ */
+
+
---
+++ Libraries/vrs_cv5.h
@@ -0,0 +1,13 @@
+/*
+ * vrs_cv5.h
+ *
+ *  Created on: 17. 10. 2016
+ *      Author: J
+ */
+
+#ifndef VRS_CV5_H_
+#define VRS_CV5_H_
+
+
+
+#endif /* VRS_CV5_H_ */
